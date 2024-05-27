8c4e7ec6fe15250a7309fcb50590ea600d1bcb0c
Babina081 <babitamoo333@gmail.com>
2024-05-27 08:38:18 +0545

    Add components component and update PostPage to display comments

---
+++ src/components/Comments.jsx
@@ -0,0 +1,40 @@
+"use client";
+import { useEffect, useState } from "react";
+import { app } from "../firebase";
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import Comment from "./Comment";
+
+
+export default function Comments({ id }) {
+  const db = getFirestore(app);
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
+
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment
+          key={comment.id}
+          comment={comment.data()}
+          id={comment.id}
+        ></Comment>
+      ))}
+    </div>
+  );
+}
